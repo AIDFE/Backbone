4dccda365130b76ad9a1a5f0e526793539f5fdd5
AIDFE <lijiaxi_1121@163.com>
2023-09-01 20:12:38 +0800

    add dataset

--- train.py
+++ train.py
@@ -1,13 +1,104 @@
-from sacred import Experiment           # Sacred 相关
-ex = Experiment('MedAI')         # Sacred 相关
+import os
+import itertools
+import glob
+import shutil
+from tools.util import AttrDict
+from torch.utils.data import DataLoader
+
+import sacred
+from sacred import Experiment
+from sacred.observers import FileStorageObserver
+from sacred.utils import apply_backspaces_and_linefeeds
+
+sacred.SETTINGS['CONFIG']['READ_ONLY_CONFIG'] = False
+sacred.SETTINGS.CAPTURE_MODE = 'no'
+
+ex = Experiment('MedAI') 
+ex.captured_out_filter = apply_backspaces_and_linefeeds
+
+source_folders = ['.', './dataloaders', './models', './my_utils']
+sources_to_save = list(itertools.chain.from_iterable(
+    [glob.glob(f'{folder}/*.py') for folder in source_folders]))
+
+for source_file in sources_to_save:
+    ex.add_source_file(source_file)
+
 
 @ex.config          # Sacred 相关
 def cfg():
-    C = 1.0
-    gamma = 0.7
 
-
-@ex.automain        # Sacred 相关
-def run(C, gamma):
+    seed = 1234
+    name = "ijepa"  # exp_name
+    checkpoints_dir = './checkpoints'
     
-    return C, gamma
+    data_name = 'ABDOMINAL'
+    tr_domain = 'MR'
+    te_domain = 'CT'
+
+    # data_name = 'MMS'
+    # tr_domain = ['vendorA']
+    # te_domain = ['vendorC']
+    
+
+
+@ex.config_hook
+def add_observer(config, command_name, logger):
+    """A hook fucntion to add observer"""
+    exp_name = f'{config["name"]}'
+    observer = FileStorageObserver.create(os.path.join(config['checkpoints_dir'], exp_name))
+    ex.observers.append(observer)
+    return config
+
+
+@ex.automain
+def main(_run, _config, _log):
+    # configs for sacred
+    if _run.observers:
+        os.makedirs(f'{_run.observers[0].dir}/snapshots', exist_ok=True)
+        for source_file, _ in _run.experiment_info['sources']:
+            os.makedirs(os.path.dirname(f'{_run.observers[0].dir}/source/{source_file}'), exist_ok=True)
+            _run.observers[0].save_file(source_file, f'source/{source_file}')
+        shutil.rmtree(f'{_run.observers[0].basedir}/_sources')
+
+        _config['run_dir'] = _run.observers[0].dir
+        _config['snapshot_dir'] = f'{_run.observers[0].dir}/snapshots'
+
+    opt = AttrDict(_config)
+
+    # load dataset
+    if opt.data_name == 'ABDOMINAL':
+        import dataloaders.abd_dataset as ABD
+        if not isinstance(opt.tr_domain, list):
+            opt.tr_domain = [opt.tr_domain]
+            opt.te_domain = [opt.te_domain]
+
+        train_set       = ABD.get_training(modality = opt.tr_domain)
+        if opt.te_domain[0] == opt.tr_domain[0]:
+            test_set        = ABD.get_test(modality = opt.te_domain, norm_func = train_set.normalize_op) # if same domain, then use the normalize op from the source
+        else:
+            test_set        = ABD.get_test_all(modality = opt.te_domain, norm_func = None)
+        label_name          = ABD.LABEL_NAME
+    
+    elif opt.data_name == 'MMS':
+        import dataloaders.mms_dataset as MMS
+        if not isinstance(opt.tr_domain, list):
+            opt.tr_domain = [opt.tr_domain]
+            opt.te_domain = [opt.te_domain]
+
+        train_set       = MMS.get_training(modality = opt.tr_domain)
+        if opt.te_domain[0] == opt.tr_domain[0]:
+            test_set        = MMS.get_test(modality = opt.te_domain, norm_func = train_set.normalize_op) # if same domain, then use the normalize op from the source
+        else:
+            test_set        = MMS.get_test_all(modality = opt.te_domain, norm_func = None)
+        label_name          = MMS.LABEL_NAME
+    
+    else:
+        raise NotImplementedError 
+    
+
+    _log.info(f'Using TR domain {opt.tr_domain}; TE domain {opt.te_domain}')
+    
+
+
+
+    
